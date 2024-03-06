15e072d72c7bbe93825751df0b170cad186acbbf
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 13:54:26 +0530

    Update LocalThemedBox.js

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+const LocalThemedBox = (props) => {
+    const [loaclTheme, setLlocalTheme] = useState(props.theme);
+    const {theme} = props;
 
-const LocalThemedBox = () => {
-
+    useEffect(()=>{
+        setLlocalTheme(theme);
+    },[theme]);
 return(
-    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
+    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box" className={`bg-${loaclTheme}`}>
         {/* Write code below this line */}
-        
+        <p id="local-themed-text-container" className={`txt-${loaclTheme}`}>
+        lorem ipsum dolor iterit n stuff
+        </p>
+        <button id="local-theme-toggler" onClick={()=>{setLlocalTheme(loaclTheme==="light"?"dark":"light")}} className={`btn btn-${loaclTheme} txt-${loaclTheme}`}>{`Toggle local theme to ${loaclTheme==="light"?"dark":"light"}`}</button>
     </div>
 )
 }
 
-export { LocalThemedBox }
+export {LocalThemedBox}
